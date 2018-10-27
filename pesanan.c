6a42146dbb347955aa61ad58ff244a02b2a6e215
Frankuy <fhizbullah99@gmail.com>
2018-10-27 17:29:15 +0700

    Definisi Pesanan,MakeEmpty, TambahPesanan, PrintPesanan

--- pesanan.c
+++ pesanan.c
@@ -1,20 +1,29 @@
 #include "pesanan.h"
 #include "pelanggan.h"
 #include <string.h>
+#include <stdio.h>
 
-void MakeEmpty (Pesanan * T)
+void MakeEmpty(Pesanan * T)
 /* I.S. T sembarang */
 /* F.S. Terbentuk tabel T kosong dengan kapasitas IdxMax-IdxMin+1 */
 {
 	Neff(*T) = 0;
 }
 
-void TambahPesanan(Pelanggan *P, int NomorMeja, Pesanan * T)
+void TambahPesanan(Pelanggan *P, int NomorMeja, int Ruangan, Pesanan *T)
 /* I.S. P Terdefinisi; Nama Makanan, Jumlah Pelangga */
 /* F.S. Pesanan ditambahkan kedalam array Pesanan */  
 {
 	Neff(*T)++;
-	strcpy(*P.makanan,Makanan(*T, Neff(*T)));
+	strcpy((*P).makanan,Makanan(*T, Neff(*T)));
 	Meja(*T, Neff(*T)) = NomorMeja;
-	Jumlah_Pesan(*T, Neff(*T)) = *P.jumlah;
+	Ruangan(*T, Neff(*T)) = Ruangan;
+	Jumlah_Pesan(*T, Neff(*T)) = (*P).jumlah;
+}
+
+void PrintPesanan(Pesanan P){
+	for (int i = 1; i <= Neff(P); i++) {
+		printf("%s, %d : Meja %d di Ruang %d\n", Makanan(P,i), Jumlah_Pesan(P,i), Meja(P,i), Ruangan(P,i));
+	}
+
 }
--- pesanan.h
+++ pesanan.h
@@ -2,6 +2,7 @@
 #define _PESANAN_H
 
 #include "boolean.h"
+#include "pelanggan.h"
 
 #define IdxMax 100
 /* Indeks maksimum array, sekaligus ukuran maksimum array dalam C */
@@ -11,6 +12,7 @@
 typedef struct {
 	char makanan[50]; /* Nama pesanan */
 	int meja; /* nomor meja yang memesan */
+	int ruangan;
 	int jumlah; /* jumlah pesanan */
 } ElType; 
 
@@ -19,19 +21,21 @@ typedef struct {
 	int Neff;
 } Pesanan;
 
-#define Makanan(P, i) Pesanan.TabPesan[i].makanan
-#define Meja(P, i) Pesanan.TabPesan[i].meja
-#define Jumlah_Pesan(P, i) Pesanan.TabPesan[i].jumlah
-#define Neff(P) Pesanan.Neff
+#define Makanan(P, i) (P).TabPesan[i].makanan
+#define Meja(P, i) (P).TabPesan[i].meja
+#define Ruangan(P, i) (P).TabPesan[i].ruangan
+#define Jumlah_Pesan(P, i) (P).TabPesan[i].jumlah
+#define Neff(P) (P).Neff
 
 void MakeEmpty(Pesanan * T);
 /* I.S. T sembarang */
 /* F.S. Terbentuk tabel T kosong dengan kapasitas IdxMax-IdxMin+1 */
 
-void TambahPesanan(Pelanggan *P, int NomorMeja, Pesanan * T);
+void TambahPesanan(Pelanggan *P, int NomorMeja, int Ruangan, Pesanan * T);
 /* I.S. P Terdefinisi; Nama Makanan, Jumlah Pelangga */
 /* F.S. Pesanan ditambahkan kedalam array Pesanan */   
 
-void KurangPesanan();
-
+void PrintPesanan(Pesanan T);
+/* I.s T sudah terdefinisi */
+/* F.s Semua pesanan akan tercetak */
 #endif

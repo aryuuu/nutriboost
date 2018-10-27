377ce67e46f71bd8cc8163041844f6fa065580ec
irhw110 <harisw110@gmail.com>
2018-10-27 16:41:49 +0700

    -_-

--- pelanggan.h
+++ pelanggan.h
@@ -3,6 +3,9 @@
 
 #include "boolean.h"
 
+#define MaxEl 10
+#define Nil 0
+
 typedef struct {
 	int jumlah;
 	int sabar;
@@ -10,7 +13,48 @@ typedef struct {
   boolean layan;
 } Pelanggan;
 
+typedef struct { Pelanggan T[MaxEl+1];   /* tabel penyimpan elemen */
+                 int HEAD;  /* alamat penghapusan */
+                 int TAIL;  /* alamat penambahan */
+               } Queue;
+/* Definisi Queue kosong: HEAD=Nil; TAIL=Nil. */
+/* Catatan implementasi: T[0] tidak pernah dipakai */
 
+/* ********* AKSES (Selektor) ********* */
+/* Jika Q adalah Queue, maka akses elemen : */
+#define Head(Q) (Q).HEAD
+#define Tail(Q) (Q).TAIL
+#define InfoHead(Q) (Q).T[(Q).HEAD]
+#define InfoTail(Q) (Q).T[(Q).TAIL]
 
+/* ********* Salin Pelanggan ********* */
+void SalinPelanggan(Pelanggan *P1,Pelanggan P2);
+/*Menyalin P2 ke P1*/
+
+/* ********* Prototype ********* */
+boolean IsEmpty (Queue Q);
+/* Mengirim true jika Q kosong: lihat definisi di atas */
+boolean IsFull (Queue Q);
+/* Mengirim true jika tabel penampung elemen Q sudah penuh */
+/* yaitu mengandung elemen sebanyak MaxEl */
+
+/* *** Kreator *** */
+void CreateEmpty (Queue * Q);
+/* I.S. sembarang */
+/* F.S. Sebuah Q kosong terbentuk dan salah satu kondisi sbb: */
+/* Jika alokasi berhasil, Tabel memori dialokasi berukuran Max+1 */
+/* atau : jika alokasi gagal, Q kosong dg MaxEl=0 */
+/* Proses : Melakukan alokasi, membuat sebuah Q kosong */
+
+/* *** Primitif Add/Delete *** */
+void Add (Queue * Q, Pelanggan X);
+/* Proses: Menambahkan X pada Q dengan aturan FIFO */
+/* I.S. Q mungkin kosong, tabel penampung elemen Q TIDAK penuh */
+/* F.S. X menjadi TAIL yang baru, TAIL "maju" dengan mekanisme circular buffer */
+void Del (Queue * Q, Pelanggan * X);
+/* Proses: Menghapus X pada Q dengan aturan FIFO */
+/* I.S. Q tidak mungkin kosong */
+/* F.S. X = nilai elemen HEAD pd I.S., HEAD "maju" dengan mekanisme circular buffer;
+        Q mungkin kosong */
 
 #endif

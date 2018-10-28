700ccb0629ba14a7d1bd177d6de42cc5e18b1ffa
fattah <herppratama@gmail.com>
2018-10-28 10:59:12 +0700

    perasaan gaada yang diganti

--- mruangan.c
+++ mruangan.c
@@ -8,8 +8,12 @@ int main(int argc, char const *argv[])
 	CreateRuangan(&Kosan,"Kosan",3,4);//bikin kamar kosong
 
 	Objek Meja = CreateObjek('M',2,false);//bikin meja baru
+	Objek Kursi = CreateObjek('K',1,false);//bikin kursi baru
+	Objek Costumer = CreateObjek('C',0,true)
 
-	SetObjek(&Kosan,Meja,1,1);//taruh meja di posisi 1,1
+
+
+	//SetObjek(&Kosan,Meja,1,1);//taruh meja di posisi 1,1
 
 	CetakRuangan(Kosan);
 	return 0;
